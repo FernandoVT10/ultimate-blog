9c27b5f28738c8006be9ef3126ce1187597f441d
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2022-12-16 19:47:44 -0600

    More cors config

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,14 +5,22 @@ import cors from "cors";
 import apiRouter from "./api";
 import errorHandler from "./middlewares/errorHandler";
 
-import { STATIC_DIR } from "./config/constants";
+import { STATIC_DIR, PRODUCTION } from "./config/constants";
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors());
+
+if(!PRODUCTION) {
+  // cors is not gonna be needed on production, because ngnix will be used
+  // to run both backend and frontend in the same domain
+  app.use(cors({
+    origin: ["http://localhost:3000"],
+    credentials: true
+  }));
+}
 
 app.use("/static/", express.static(STATIC_DIR));
 
