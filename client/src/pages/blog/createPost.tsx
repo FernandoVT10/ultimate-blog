6570833b2cfe728275d875a221d2bb047af047b9
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2023-02-15 16:56:13 -0600

    Super basic page securing added

--- client/src/pages/blog/createPost.tsx
+++ client/src/pages/blog/createPost.tsx
@@ -1,6 +1,30 @@
+import { GetServerSideProps } from "next";
+import { checkAdminStatusFromServer } from "@services/AdminService";
+import { AUTH_COOKIE_KEY } from "@config/constants";
+
 import CreateBlogPostForm from "@domain/CreatePost";
 import Header from "@components/Header";
 import Head from "next/head";
+
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const authToken = req.cookies[AUTH_COOKIE_KEY];
+  const isAdmin = await checkAdminStatusFromServer(authToken);
+
+  if(!isAdmin) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/"
+      },
+      props: {}
+    };
+  }
+
+  return {
+    props: {}
+  };
+};
 
 export default function CreateBlogPostPage() {
   return (
