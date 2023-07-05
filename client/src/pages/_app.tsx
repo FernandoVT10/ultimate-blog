6eb69d3ec0c49cd2e8139bfb8efd12c05db4c20a
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2023-07-04 19:02:59 -0600

    Favicon added

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -13,7 +13,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
