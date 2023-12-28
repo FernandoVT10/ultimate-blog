06d8c755bfd9ee4d4772bb20314305df737b1085
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2023-12-27 21:03:52 -0600

    Changing some styles

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,12 +1,29 @@
+import Image from "next/image";
+import Navbar from "@components/Navbar";
+
+import styles from "./Header.module.scss";
+
 interface HeaderProps {
   children?: JSX.Element;
   height?: number;
 }
 
-export default function Header(a: HeaderProps) {
-  a.height;
-
+export default function Header({ children, height }: HeaderProps) {
   return (
-    <h1>REMOVE COMPONENT</h1>
+    <header className={styles.header} style={{ height: height || 50 }}>
+      <Navbar/>
+
+      <Image
+        src="/images/header-bg.webp"
+        alt="Header Image"
+        className={styles.bg}
+        priority
+        fill
+      />
+
+      <div className={styles.children}>
+        { children }
+      </div>
+    </header>
   );
 }
